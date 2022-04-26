14410ab66cec2c0f848976c4932c6fff753746aa
sahana178 <85974914+sahana178@users.noreply.github.com>
2022-04-25 20:42:59 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+leftWristX = 0;
+rightWristX = 0;
+noseX = 0;
+noseY = 0;
+difference = 0;
+
+function setup(){
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw(){
+    background("#c9b6db");
+    fill("#5c5f87");
+    stroke("#5c5f87");
+    square(noseX, noseY, difference);
+    
+}
+
+function modelLoaded(){
+    console.log('model is loaded');
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log(" noseX = " + noseX + " noseY = " + noseY );
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = leftWristX - rightWristX;
+        console.log(" leftWristX = " + leftWristX + " rightWristX = " + rightWristX + " difference = " + difference);
+    }
+}
+
